d573194326ee0dd698c371156dcf8a4778122722
Perceval <dnamella1@hotmail.com>
2018-07-26 14:35:02 +0200

    Loyse scene arrangement

---
+++ SGA - Twix Gaming/Assets/Scripts/ADR_GUIProgressBar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ADR_GUIProgressBar : MonoBehaviour {
+
+    public Image progressBar;
+    public Text display;
+
+    public void setProgress(float amount) {
+        amount = Mathf.Clamp01(amount);
+        progressBar.fillAmount = amount;
+        display.text = ((int)(amount * 100))  + "%";
+    }
+}
---
+++ SGA - Twix Gaming/Assets/Scripts/ADR_MainScreen.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ADR_MainScreen : MonoBehaviour {
+
+    public ADR_LevelManager levelManager;
+    public Canvas canvas;
+    public Text mainScreen;
+    public Text restartButtonText;
+    public Text exitButtonText;
+
+    public Color winColor;
+    public Color loseColor;
+
+    private void Start() {
+        canvas.gameObject.SetActive(false);
+    }
+
+    public void Exit() {
+        Application.Quit();
+    }
+
+    public void Restart() {
+        levelManager.Reset();
+        canvas.gameObject.SetActive(false);
+    }
+
+    public void Win() {
+        
+        mainScreen.color = winColor;
+        mainScreen.text = "Success !";
+        restartButtonText.color = winColor;
+        exitButtonText.color = winColor;
+        canvas.gameObject.SetActive(true);
+    }
+
+    public void Lose() {
+
+        mainScreen.color = loseColor;
+        mainScreen.text = "Failure !";
+        restartButtonText.color = loseColor;
+        exitButtonText.color = loseColor;
+        canvas.gameObject.SetActive(true);
+    }
+}
