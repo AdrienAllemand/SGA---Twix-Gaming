950bcba9c0de5186f6eb4d0bf2c6e9ba4446aeb9
Unknown <lloysekrug@gmail.com>
2018-07-27 14:18:05 +0200

    menu fin à tester

---
+++ SGA - Twix Gaming/Assets/Scripts/LoadingScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+ 
+
+public class LoadingScene : MonoBehaviour {
+
+    [SerializeField]
+    private string sceneName;
+
+	public void call()
+    {
+        Debug.Log("Clic!");
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
